0d52e97c28ccfd64937ef33f687f0d7eb8775b42
Christian Marhoff <chris@marhoff.de>
2025-06-22 15:31:31 +0200

    mehr Übungen

--- data.js
+++ data.js
@@ -2,5 +2,18 @@ const exercises = [
   { name: "Flutter Kicks", display: "10 secs", initial: 10 },
   { name: "Push Ups", display: "8 reps", initial: 0 },
   { name: "Vertical Jumps", display: "8 reps", initial: 0 },
-  { name: "Plank", display: "20 secs", initial: 20 }
+  { name: "Plank", display: "20 secs", initial: 20 },
+  { name: "Mountain Climber", display: "15 secs", initial: 15 },
+  { name: "Glute Bridge", display: "15 reps", initial: 0 },
+  { name: "High Knees", display: "15 secs", initial: 15 },
+  { name: "T-Rotation Plank", display: "10 reps", initial: 0 },
+  { name: "Jumping Jacks", display: "25 secs", initial: 25 },
+  { name: "Side Plank Left Side", display: "20 secs", initial: 20 },
+  { name: "Burpees", display: "6 reps", initial: 0 },
+  { name: "Side Plank Right Side", display: "20 secs", initial: 20 },
+  { name: "Good Mornings", display: "10 reps", initial: 0 },
+  { name: "Pike Push Up", display: "4 reps", initial: 0 },
+  { name: "Plank", display: "20 secs", initial: 20 },
+  { name: "Diamond Push Up", display: "3 reps", initial: 0 },
+  { name: "Flutter Kicks", display: "10 secs", initial: 10 }
 ];
